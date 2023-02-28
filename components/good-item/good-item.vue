bc40910541275b078f68ced74a0434924ba423be
linchuyao6 <2228197551@qq.com>
2023-02-28 20:52:50 +0800

    完成了商品列表页面的开发

---
+++ components/good-item/good-item.vue
@@ -0,0 +1,74 @@
+<template>
+  <view>
+    <view class="good_item">
+
+      <!-- 左侧盒子部分 -->
+      <view class="good_item_left">
+        <image :src="goods.goods_small_logo||defultPic"></image>
+      </view>
+      <view class="good_item_right">
+        <view class="good_item_name">{{goods.goods_name}}</view>
+        <view class="good_item_price">
+          <view class="price">￥{{goods.goods_price|toFixed}}</view>
+        </view>
+
+      </view>
+    </view>
+  </view>
+
+</template>
+
+<script>
+  export default {
+    name: "good-item",
+    data() {
+      return {
+        defultPic: 'http://image3.suning.cn/uimg/b2c/newcatentries/0070061155-000000000617064184_1_200x200.jpg'
+      };
+    },
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    filters: {
+      toFixed(res) {
+        return Number(res).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods_list {
+    .good_item {
+      display: flex;
+      padding: 10px 5px;
+      border-bottom: 1px solid #efefef;
+
+      .good_item_left {
+        image {
+          width: 100px;
+          height: 100px;
+          margin-right: 5px;
+        }
+      }
+
+      .good_item_right {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .good_item_name {}
+
+        .good_item_price {
+          .price {
+            color: #c00;
+            font-size: 17px;
+          }
+        }
+      }
+    }
+  }
+</style>
--- subpackge/good_list/good_list.vue
+++ subpackge/good_list/good_list.vue
@@ -1,6 +1,12 @@
 <template>
   <view>
-    
+    <view class="goods_list">
+      <block v-for="(item,i) in goodsList" :key="i">
+      <good-item :goods="item"></good-item>
+      </block>
+    </view>
+   
+
   </view>
 </template>
 
@@ -8,12 +14,60 @@
   export default {
     data() {
       return {
+        obj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10,
+        },
+        goodsList: [],
+        total: 0,
+        isLoading:false,
         
-      };
+      }
+    },
+    onReachBottom() {
+      if(this.obj.pagesize*this.obj.pagenum>this.total) return uni.$showMsg('无更多数据')
+      if(this.isLoading===true) return
+      this.obj.pagenum+=1
+      this.getGoodList()
+    },
+    onLoad(option) {
+      console.log(option);
+      this.obj.query = option.query || '';
+      this.obj.cid = option.cid || '';
+      this.getGoodList();
+    },
+    onPullDownRefresh() {
+      this.obj.pagenum=0;
+      this.total=0;
+      this.goodsList=[];
+      this.isLoading=false;
+      // 这里采用回调函数的形式在调用完相关函数时关闭下拉刷新。
+      this.getGoodList(()=>uni.stopPullDownRefresh())
+    },
+    methods: {
+      async getGoodList(cb) {
+        this.isLoading=true
+        const {
+          data: res
+        } = await uni.$http.get('/goods/search?pagenum=',this.obj)
+        this.isLoading=false
+        // 这里如果传入了参数，在请求完数据后立刻执行回调函数，阻止下拉刷新
+        
+        cb&&cb()
+        if (res.meta.status !== 200) {
+          return uni.$showMsg()
+        }
+        console.log(res);
+        this.goodsList = [...this.goodsList,...res.message.goods]
+        this.total=res.message.total
+        
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+  
 </style>
